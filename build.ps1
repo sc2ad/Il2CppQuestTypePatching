5369d8ae9913de1abc97f973a420b376ca876f96
RedBrumbler <redbrumbler.official+github@gmail.com>
2021-12-18 23:22:51 +0100

    Convert to cmake + qom-rust

--- build.ps1
+++ build.ps1
@@ -1,8 +1,38 @@
-$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+Param(
+    [Parameter(Mandatory=$false)]
+    [Switch]$clean,
+    [Parameter(Mandatory=$false)]
+    [Switch]$local_test,
+    [Parameter(Mandatory=$false)]
+    [Switch]$local_test_coroutine
+)
 
-$buildScript = "$NDKPath/build/ndk-build"
-if (-not ($PSVersionTable.PSEdition -eq "Core")) {
-    $buildScript += ".cmd"
+# if user specified clean, remove all build files
+if ($clean.IsPresent)
+{
+    if (Test-Path -Path "build")
+    {
+        remove-item build -R
+    }
 }
 
-& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+
+if (($clean.IsPresent) -or (-not (Test-Path -Path "build")))
+{
+    $out = new-item -Path build -ItemType Directory
+}
+
+cd build
+if ($local_test.IsPresent) {
+    & cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Release" -DLOCAL_TEST=a ../
+} elseif ($local_test_coroutine.IsPresent) {
+    & cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Release" -DLOCAL_TEST_COROUTINE=a../
+} else {
+    & cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Release" ../
+}
+
+& cmake --build .
+$ExitCode = $LastExitCode
+cd ..
+exit $ExitCode
